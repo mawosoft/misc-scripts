5ba0780af9f0d5eb2bda8214458c088166e56fe6
mawosoft <78562192+mawosoft@users.noreply.github.com>
2023-10-15 19:07:10 +0200

    [pwsh profile] Add PSReadLine key handler

--- PowerShell/Profile/Common/statements.ps1
+++ PowerShell/Profile/Common/statements.ps1
@@ -9,3 +9,23 @@ Set-PSReadLineOption -HistorySavePath (Join-Path (
     ) 'PowerShell\PSReadLine\AllHosts_history.txt' )
 # Can be toggled with F2.
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+Set-PSReadLineKeyHandler -Chord Ctrl+F1 -BriefDescription 'Online Help' -Description 'Show online help for the command under or before the cursor.' -ScriptBlock {
+    param($key, $arg)
+    [System.Management.Automation.Language.Token[]]$tokens = $null
+    [int]$cursor = 0
+    [string]$commandName = ''
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$null, [ref]$tokens, [ref]$null, [ref]$cursor)
+    $null = $null
+    #Write-Warning 'debug' -WarningAction Break
+    foreach ($token in $tokens) {
+        if ($token.Extent.StartOffset -gt $cursor) { break }
+        if ($token.TokenFlags -band [System.Management.Automation.Language.TokenFlags]::CommandName) {
+            $commandName = $token.Text
+        }
+    }
+    if ($commandName) {
+        # -ErrorAction Ignore has no effect
+        try { $null = Get-Help $commandName -Online } catch {}
+    }
+}
